859d51722ddda83a03e60d70e53c37ee27847181
Isaac <isaakcovarrubias@gmail.com>
2023-06-27 10:27:09 -0400

    Iniciando actividades del dia

--- Carrito de compras/js/app.js
+++ Carrito de compras/js/app.js
@@ -3,7 +3,7 @@ const carrito = document.querySelector('#carrito');
 const listaCursos = document.querySelector('#lista-cursos');
 const listaCarrito = document.querySelector('#lista-carrito tbody');
 const vaciarCarritoButton = document.querySelector('#vaciar-carrito');
-
+let articulosCarrito = [];
 // Funciones
 function registrarEventListeners()
 {
@@ -32,7 +32,40 @@ function leerDatosCurso(curso)
         precio: curso.querySelector('.precio span').textContent,
         id: curso.querySelector('a').getAttribute('data-id'),
         cantidad: 1
+    }
+    // Agreaga elementos al arreglo carrito
+    articulosCarrito = [...articulosCarrito, infoCurso]
+}
 
+// Muestra el carrito de compras en el HTML
+function carritoHTML()
+{
+    // Limpiar el HTML para eliminar duplicados
+    limpiarHTML();
+    // Recorre el Carrito de compras y genera el HTML
+    articulosCarrito.forEach
+    (curso =>
+    {
+        const row = document.createElement('tr')
+        row.innerHTML = 
+        `
+        <td><img src = "${curso.imagen}" width = "100"></td>
+        <td>${curso.titulo}</td>
+        
+        `;
 
+        //Agrega el HTML del carrito en el tbody
+        contenerdorCarrito.appendChild(row);
+    })
+}
+
+// Elimina los cursos del tbody
+function limpiarHTML()
+{
+    // Forma lenta
+    //contenedorCarrito.innerHTML = '';
+    while(contenedorCarrito.firstChild)
+    {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild)
     }
 }
